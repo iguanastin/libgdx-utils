2c5648ca1f25530d394383762db87f460ae3e808
Austin Thompson <austinthompson194@gmail.com>
2016-09-10 23:50:17 -0600

    Modified GameMain to fit update and render flow.

--- core/src/game/main/GameMain.java
+++ core/src/game/main/GameMain.java
@@ -5,23 +5,46 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import utils.gui.Gui;
 
 public class GameMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        Gui.resetRenderers();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        Gui.resetRenderers();
+    }
+
+    @Override
+    public void dispose() {
+        Gui.dispose();
+    }
+
+    public void act(float delta) {
+
+    }
+
+    @Override
+    public void render() {
+        //Act
+        act(Gdx.graphics.getDeltaTime());
+
+        //Clear background
+        Gdx.gl.glClearColor(Gui.background_color.r, Gui.background_color.g, Gui.background_color.b, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        //Start batch
+        Gui.begin(Gui.batch);
+
+        //Render things here
+        
+
+        //End all renderers
+        Gui.end(Gui.batch);
+        Gui.end(Gui.sr);
+    }
 }
